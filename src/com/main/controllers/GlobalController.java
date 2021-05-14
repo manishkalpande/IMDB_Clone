7e4de3f9028e4baa8822c1e2d091746eec3a043e
manishkalpande <manishkalpande23@gmail.com>
2021-05-14 23:40:11 +0530

    ready project

---
+++ src/com/main/controllers/GlobalController.java
@@ -0,0 +1,16 @@
+package com.main.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GlobalController 
+{
+	@RequestMapping("/")
+	 public String home()
+	 {
+	 return "index";
+	 }
+
+
+}
